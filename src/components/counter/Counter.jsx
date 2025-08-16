0d1769c2a3a32fb3fbdfeabfae59cf751f4e63a2
Amirh86-dev <birdwolf913@gmail.com>
2025-08-16 22:30:53 +0330

    fix: fix the error photo not found

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,20 +1,49 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Counter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0,
+    };
+  }
+  IncreaseCounter() {
+    // console.log(this.setState());
+
+    this.setState((per) => {
+      return { counter: per.counter + 1 };
+    });
+  }
+  DecreaseCounter() {
+    // console.log("remove");
+
+    this.setState((per) => {
+      return { counter: per.counter <= 0 ? 0 : per.counter - 1};
+    });
+  }
   render() {
     return (
-      <div className='container'>
-        <div className='counter-container'>
-            <div className='counter-text'>
-                <h1 className='counter-text__title'>counter</h1>
-                <h4  className='counter-text__desc'>0</h4>
+      <div className="container">
+        <div className="counter-container">
+          <div className="counter-text">
+            <h1 className="counter-text__title">counter</h1>
+            <h4 className="counter-text__desc">{this.state.counter}</h4>
+          </div>
+          <div className="counter-btns">
+            <div className="counter-btns__btn-increase">
+              <button onClick={() => this.IncreaseCounter()} className="btn">
+                Increase
+              </button>
             </div>
-            <div className='counter-btns'>
-                <div className='counter-btns__btn-increase'><button className='btn'>Increase</button></div>
-                <div className='counter-btns__btn-decrease'><button className='btn'>decrease</button></div>
+            <div className="counter-btns__btn-decrease">
+              <button onClick={() => this.DecreaseCounter()} className="btn">
+                decrease
+              </button>
             </div>
+          </div>
         </div>
       </div>
-    )
+    );
   }
 }
