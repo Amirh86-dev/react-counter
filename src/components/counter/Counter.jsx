48bf49cf68a153d39253dbbeb1b4cd7212c6c1f8
Amirh86-dev <birdwolf913@gmail.com>
2025-08-16 22:11:43 +0330

    feat: complate ui

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -3,7 +3,18 @@ import React, { Component } from 'react'
 export default class Counter extends Component {
   render() {
     return (
-      <div></div>
+      <div className='container'>
+        <div className='counter-container'>
+            <div className='counter-text'>
+                <h1 className='counter-text__title'>counter</h1>
+                <h4  className='counter-text__desc'>0</h4>
+            </div>
+            <div className='counter-btns'>
+                <div className='counter-btns__btn-increase'><button className='btn'>Increase</button></div>
+                <div className='counter-btns__btn-decrease'><button className='btn'>decrease</button></div>
+            </div>
+        </div>
+      </div>
     )
   }
 }
